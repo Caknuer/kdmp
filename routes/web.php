5558d3e5b72efb9f392681a4eaa075b2fa136179
oy <caknuroy@gmail.com>
2025-11-21 23:02:25 +0700

    routing

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+use App\Http\Controllers\PublicController;
+
+Route::get('/', [PublicController::class, 'home'])->name('home');
+
+Route::get('/profil/{slug}', [PublicController::class, 'profile'])->name('profile');
+
+Route::get('/unit-bisnis', [PublicController::class, 'businessUnits'])->name('business.units');
+Route::get('/unit-bisnis/{slug}', [PublicController::class, 'businessDetail'])->name('business.unit.detail');
+
+Route::get('/mitra', [PublicController::class, 'partners'])->name('partners');
+
+Route::get('/berita', [PublicController::class, 'articles'])->name('articles');
+Route::get('/berita/{slug}', [PublicController::class, 'articleDetail'])->name('articles.detail');
+
+Route::get('/transparansi', [PublicController::class, 'finance'])->name('finance');
