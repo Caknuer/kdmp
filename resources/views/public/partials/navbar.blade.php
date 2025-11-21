6baf972f4f8c520e98c1c4dbd23f69e7416517cd
oy <caknuroy@gmail.com>
2025-11-21 22:59:50 +0700

    navbar

---
+++ resources/views/public/partials/navbar.blade.php
@@ -0,0 +1,14 @@
+<nav class="bg-white shadow">
+    <div class="container mx-auto px-4 py-4 flex justify-between">
+        <a href="/" class="text-xl font-bold">KDMP</a>
+
+        <div class="flex gap-4">
+            <a href="/" class="hover:text-blue-600">Beranda</a>
+            <a href="/profil/tentang" class="hover:text-blue-600">Profil</a>
+            <a href="/unit-bisnis" class="hover:text-blue-600">Unit Bisnis</a>
+            <a href="/mitra" class="hover:text-blue-600">Mitra</a>
+            <a href="/berita" class="hover:text-blue-600">Berita</a>
+            <a href="/transparansi" class="hover:text-blue-600">Transparansi</a>
+        </div>
+    </div>
+</nav>
