7b39b2712f791538c88f863c79aa1a6bc2118bfa
oy <caknuroy@gmail.com>
2025-11-27 13:38:27 +0700

    tampilan berita step1

--- resources/views/public/articles/index.blade.php
+++ resources/views/public/articles/index.blade.php
@@ -1,3 +1,11 @@
-<h1 class="text-2xl font-bold">{{ $article->title }}</h1>
-<img src="{{ asset('storage/'.$article->thumbnail) }}" class="my-4"/>
-<p>{!! $article->body !!}</p>
+@foreach ($articles as $article)
+    <h1 class="text-2xl font-bold">{{ $article->title }}</h1>
+    <img src="{{ asset('storage/' . $article->thumbnail) }}" class="my-4"/>
+    <p>{{ Str::limit(strip_tags($article->body), 200) }}</p>
+
+    <a href="{{ route('public.articles.show', $article->slug) }}" class="text-blue-600">
+        Baca selengkapnya →
+    </a>
+
+    <hr class="my-6">
+@endforeach
