0a24167f52847d970b4e9f082b715134098d355d
oy <caknuroy@gmail.com>
2025-11-29 19:09:38 +0700

    tampilan besik

--- resources/views/public/articles/detail.blade.php
+++ resources/views/public/articles/detail.blade.php
@@ -1,3 +1,8 @@
+@extends('layouts.public')
+
+@section('P')
+    
 <h1 class="text-2xl font-bold">{{ $article->title }}</h1>
 <img src="{{ asset('storage/'.$article->thumbnail) }}" class="my-4"/>
 <p>{!! $article->body !!}</p>
+@endsection
--- resources/views/public/articles/index.blade.php
+++ resources/views/public/articles/index.blade.php
@@ -1,11 +1,9 @@
-@foreach ($articles as $article)
-    <h1 class="text-2xl font-bold">{{ $article->title }}</h1>
-    <img src="{{ asset('storage/' . $article->thumbnail) }}" class="my-4"/>
-    <p>{{ Str::limit(strip_tags($article->body), 200) }}</p>
+@extends('layouts.public')
 
-    <a href="{{ route('public.articles.show', $article->slug) }}" class="text-blue-600">
-        Baca selengkapnya →
-    </a>
+@section('P')
+        {{-- <h1 class="text-2xl font-bold">{{ article->title }}</h1>
+        <img src=" asset('storage/' .article->thumbnail) }}" class="my-4"/>
+        <p>{{ \Illuminate\Support\Str::limit(strip_tags(article->body), 200)}} </p> --}}
 
-    <hr class="my-6">
-@endforeach
+        <h1> Berita Hari ini</h1>
+@endsection
