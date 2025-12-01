f26ce240542f22e32a9910ed16692e060f5e26d6
oy <caknuroy@gmail.com>
2025-12-01 18:24:43 +0700

    bener hal utama

---
+++ resources/views/public/partials/hero.blade.php
@@ -0,0 +1,12 @@
+<section class="bg-cover bg-center h-[350px] md:h-[450px] flex items-center"
+    style="background-image: url('/images/hero-kdmp.jpg')">
+
+    <div class="bg-black/40 w-full h-full flex items-center">
+        <div class="container mx-auto px-4 text-white">
+            <h1 class="text-4xl md:text-5xl font-bold">Koperasi Desa Merah Putih</h1>
+            <p class="mt-4 text-lg md:text-xl w-full md:w-2/3">
+                Mewujudkan Kemandirian Ekonomi Desa melalui Kolaborasi, Inovasi, dan Kesejahteraan Bersama.
+            </p>
+        </div>
+    </div>
+</section>
