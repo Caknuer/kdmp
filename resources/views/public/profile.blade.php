bcdfd5d1b5c5d723b62321431dfa829249bb1e02
oy <caknuroy@gmail.com>
2025-12-02 15:29:48 +0700

    css profil

--- resources/views/public/profile.blade.php
+++ resources/views/public/profile.blade.php
@@ -1,10 +1,8 @@
 @extends('layouts.public')
 
 @section('P')
-    <div class="container mx-auto px-4 py-10 prose max-w-none">
-    <h1 class="text-3xl font-bold mb-4">{{ $data->title }}</h1>
-
-    <div class="prose max-w-none">
-        {!! $data->content !!}
+    <div class="container section">
+        <h1 class="page-title">{{ $data->title }}</h1>
+        <div class="content-body">{!! $data->content !!}</div>
     </div>
 @endsection
