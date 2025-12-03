72c02b058c1845fc7ebecdfe160bdc8ebbe61cb2
oy <caknuroy@gmail.com>
2025-12-03 11:03:35 +0700

    penyesuaian css

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -4,18 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title ?? 'KDMP Wonokerto' }}</title>
-    <link rel="stylesheet" href="/css/style.css">
+
+    <!-- Load CSS Manual -->
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
-<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-<body class="pt-20 bg-gray-50 text-gray-800">
+
+<body>
 
     @include('public.partials.navbar')
 
-    <main class="min-h-screen">
-
+    <main class="main-content">
         @yield('P')
     </main>
 
     @include('public.partials.footer')
+
 </body>
 </html>
