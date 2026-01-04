840192825b13b69f8e445ca11f6846be24613423
Cak Nur <caknur@email.com>
2026-01-04 15:56:09 +0700

    menjadikan drop down

--- resources/views/public/partials/navbar.blade.php
+++ resources/views/public/partials/navbar.blade.php
@@ -3,7 +3,20 @@
 
     <div class="menu">
         <a href="/">Beranda</a>
-        <a href="/profil">Profil</a>
+        <!-- DROPDOWN PROFIL -->
+        <li class="dropdown">
+            <a href="#" class="dropdown-toggle">
+                Profil
+                <span class="arrow">▾</span>
+            </a>
+
+            <ul class="dropdown-menu">
+                <li><a href="/profil/tentang">Tentang</a></li>
+                <li><a href="/profil/pengurus">Pengurus</a></li>
+                <li><a href="/profil/pengawas">Pengawas</a></li>
+                <li><a href="/profil/visi-misi">Visi & Misi</a></li>
+            </ul>
+        </li>
         <a href="/unit-bisnis">Unit Bisnis</a>
         <a href="/mitra">Mitra</a>
         <a href="/berita">Berita</a>
