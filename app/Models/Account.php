6cfab02f8062a02d30a5e70f6589e3f7451d9147
Cak Nur <caknur@email.com>
2026-01-11 17:23:36 +0700

    mini perbaikan

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Account extends Model
 {
-    //
+    protected $fillable = [
+        'nama',
+        'type',
+        'created_at',
+        'updated_at',
+    ];
 }
--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
 {
-    //
+    protected $fillable = [
+        'date',
+        'amount',
+        'type',
+        'description',
+    ];
+
+    protected $casts = [
+        'date' => 'date',
+    ];
 }
