4e7337fb6d487aa2600402ebd98009771fd0b3dc
Cak Nur <caknur@email.com>
2026-01-11 19:12:15 +0700

    create widget

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -5,7 +5,7 @@ namespace App\Filament\Widgets;
 use Filament\Widgets\StatsOverviewWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 use App\Models\Article;
-use App\Models\Busines;
+use App\Models\BusinessUnit;
 use App\Models\Transaction;
 
 class StatsOverview extends StatsOverviewWidget
@@ -14,7 +14,7 @@ class StatsOverview extends StatsOverviewWidget
     {
         return [
             Stat::make('Total Berita', Article::count()),
-            Stat::make('Unit Usaha', Busines::count()),
+            Stat::make('Unit Usaha', BusinessUnit::count()),
             Stat::make('Transaksi', Transaction::count()),
         ];
     }
