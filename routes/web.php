97dfcba97ad468fc791c8a76f72b0c4ee86cce99
Cak Nur <caknur@email.com>
2026-01-14 11:27:50 +0700

    route update

--- routes/web.php
+++ routes/web.php
@@ -28,4 +28,4 @@ Route::get('/berita', [PublicController::class, 'articles'])->name('articles');
 Route::get('/berita/{slug}', [PublicController::class, 'articleDetail'])->name('articles.detail');
 
 Route::get('/transparansi', [PublicController::class, 'finance'])->name('finance');
-Route::get('/transparansi-keuangan', [PublicController::class, 'index']);
+Route::redirect('/transparansi-keuangan', '/transparansi');
